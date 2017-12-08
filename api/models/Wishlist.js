baad8149652aaaa57e109793bf2c6c4d65ff1241
Simbidion <simonadwyer@yahoo.com.au>
2017-12-08 12:53:52 +1100

    add populating of products in wishlist

--- api/models/Wishlist.js
+++ api/models/Wishlist.js
@@ -5,7 +5,7 @@ const WishlistSchema = new Schema({
   // Has one/belongs to user
   user: { type: Schema.ObjectId, ref: 'User', unique: true },
   // Has many products
-  product: [{ type: Schema.ObjectId, ref: 'Product' }]
+  products: [{ type: Schema.ObjectId, ref: 'Product' }]
 })
 
 const Wishlist = mongoose.model('Wishlist', WishlistSchema)
--- api/routes/wishlist.js
+++ api/routes/wishlist.js
@@ -6,6 +6,7 @@ const router = new express.Router()
 
 router.get('/wishlist', requireJWT, (req, res) => {
   Wishlist.findOne({ user: req.user })
+    .populate('products')
     .then((wishlist) => {
       if (wishlist) {
         res.json({ products: wishlist.products })
@@ -17,6 +18,38 @@ router.get('/wishlist', requireJWT, (req, res) => {
     })
     .catch((error) => { 
       res.status(500).json({ error: error.message })
+    })
+})
+
+router.post('/wishlist/products/:productID', requireJWT, (req, res) => {
+  const { productID } = req.params
+  Wishlist.findOneAndUpdate(
+    { user: req.user }, 
+    { $addToSet: { products: productID } }, // Checks if already exists and if not adds
+    { upsert: true, new: true, runValidators: true } // Upsert: Update if exists, or else insert (create)
+  )
+    .populate('products')  
+    .then((wishlist) => {
+      res.json({ products: wishlist.products })
+    })
+    .catch((error) => {
+      res.status(400).json({ error })
+    })
+})
+
+router.delete('/wishlist/products/:productID', requireJWT, (req, res) => {
+  const { productID } = req.params
+  Wishlist.findOneAndUpdate(
+    { user: req.user }, 
+    { $pull: { products: productID } }, // Checks if already exists and if not adds
+    { upsert: true, new: true, runValidators: true } // Upsert: Update if exists, or else insert (create)
+  )
+    .populate('products')  
+    .then((wishlist) => {
+      res.json({ products: wishlist.products })
+    })
+    .catch((error) => {
+      res.status(400).json({ error })
     })
 })
 
